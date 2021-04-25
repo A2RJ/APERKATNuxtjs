868a19f2cea1385d4713380b476cf0db36b80abf
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2021-04-25 12:03:19 +0700

    Setting Number Format and Chat

--- plugins/numeral.js
+++ plugins/numeral.js
@@ -1,19 +1,5 @@
 import Vue from 'vue'
 import numeral from 'numeral';
 import numFormat from 'vue-filter-number-format';
-// import { Doughnut } from "vue-chartjs"
 
 Vue.filter('numFormat', numFormat(numeral));
-
-// export default {
-//     extends: Doughnut,
-//     props:['chartData','options'],
-//     methods: {
-//       update() {
-//         this.$data._chart.update()
-//       }
-//     },
-//     mounted () {
-//       this.renderChart(this.chartData, this.options)
-//     }
-//   };
