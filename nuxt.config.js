fb6a89929835032511d68a6f7e8c935528bee36e
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2021-05-22 08:22:43 +0700

    Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'SubmissionNuxtJS',
+    title: 'APERKAT - Universitas Teknologi Sumbawa',
     htmlAttrs: {
       lang: 'en'
     },
